e787c2d0c6b5407490e38fc80049d1a2bb7c7656
Jashanpreet Jandu <jashanjandu23@gmail.com>
2022-10-09 13:56:23 -0700

    Done!

--- src/Main.java
+++ src/Main.java
@@ -4,89 +4,61 @@
  * Assignment 1: Minesweeper
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File("minesweeper_input.txt"));
         minesweeper(input);
     }
 
     private static void minesweeper(Scanner theScan) {
         int row = theScan.nextInt();
         int col = theScan.nextInt();
-        int fMine = 1;
-        int[][] mineField = new int[row][col];
-        while (row != 0 && col != 0) {
-            for (int i = 0; i < row; i++) {
+        while(row != 0 && col != 0) {
+            char[][] board = new char[row][col];
+            for(int i = 0; i < row; i++) {
                 String line = theScan.next();
-                for (int j = 0; j < col; j++) {
-                    if (line.charAt(j) == '*') {
-                        mineField[i][j] = fMine;
-                    }
+                for(int j = 0; j < col; j++) {
+                    board[i][j] = line.charAt(j);
                 }
             }
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if (mineField[i][j] != fMine) {
-                        mineField[i][j] = countMines(mineField, i, j);
-                    }
-                }
-            }
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if (mineField[i][j] == fMine) {
-                        System.out.print("*");
+            for(int i = 0; i < row; i++) {
+                for(int j = 0; j < col; j++) {
+                    if(board[i][j] == '*') {
+                        System.out.print('*');
                     } else {
-                        System.out.print(mineField[i][j]);
+                        System.out.print(countMines(board, i, j));
                     }
                 }
                 System.out.println();
             }
+            System.out.println();
             row = theScan.nextInt();
             col = theScan.nextInt();
-            if (row != 0 && col != 0) {
-                System.out.println();
-            }
         }
     }
 
-    private static int countMines(int[][] theField, int theRow, int theCol) {
+    private static int countMines(char[][] theField, int theRow, int theCol) {
         int count = 0;
-        int fMine = 1;
-        int row = theField.length;
-        int col = theField[0].length;
-        if (theRow - 1 >= 0 && theCol - 1 >= 0 && theField[theRow - 1][theCol - 1] == fMine) {
-            count++;
-        }
-        if (theRow - 1 >= 0 && theField[theRow - 1][theCol] == fMine) {
-            count++;
-        }
-        if (theRow - 1 >= 0 && theCol + 1 < col && theField[theRow - 1][theCol + 1] == fMine) {
-            count++;
-        }
-        if (theCol - 1 >= 0 && theField[theRow][theCol - 1] == fMine) {
-            count++;
-        }
-        if (theCol + 1 < col && theField[theRow][theCol + 1] == fMine) {
-            count++;
-        }
-        if (theRow + 1 < row && theCol - 1 >= 0 && theField[theRow + 1][theCol - 1] == fMine) {
-            count++;
-        }
-        if (theRow + 1 < row && theField[theRow + 1][theCol] == fMine) {
-            count++;
-        }
-        if (theRow + 1 < row && theCol + 1 < col && theField[theRow + 1][theCol + 1] == fMine) {
-            count++;
+        for(int i = theRow - 1; i <= theRow + 1; i++) {
+            for(int j = theCol - 1; j <= theCol + 1; j++) {
+                if(i >= 0 && i < theField.length && j >= 0 && j < theField[0].length) {
+                    if(theField[i][j] == '*') {
+                        count++;
+                    }
+                }
+            }
         }
         return count;
     }
 
     private static void printMineField(int[][] theField) {
-        for (int[] ints : theField) {
-            for (int j = 0; j < theField[0].length; j++) {
-                System.out.print(ints[j]);
+        for(int i = 0; i < theField.length; i++) {
+            for(int j = 0; j < theField[0].length; j++) {
+                System.out.print(theField[i][j] + " ");
             }
             System.out.println();
         }
